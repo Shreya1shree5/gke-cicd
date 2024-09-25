93f7d104affe0a068ae4c80b6c58e0cdbb0eb64b
Shreya1shree5 <47668807+Shreya1shree5@users.noreply.github.com>
2024-09-25 23:41:33 +0530

    Update app.py

--- app1/app.py
+++ app1/app.py
@@ -1,13 +1,9 @@
 from flask import Flask
+server = Flask(__name__)
 
-# Create a Flask application
-app = Flask(__name__)
+@server.route("/")
+ def hello():
+    return "Hello World!"
 
-# Define a route for the root URL
-@app.route('/')
-def hello_world():
-    return 'Hello, World App 1!'
-
-# Run the Flask application if this file is executed directly
-if __name__ == '__main__':
-    app.run(debug=True, port=8080,host='0.0.0.0')
+if __name__ == "__main__":
+   server.run(host='0.0.0.0')
